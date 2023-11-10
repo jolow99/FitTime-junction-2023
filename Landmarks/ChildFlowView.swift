aedc2afac13cc6a85b7c05d58d4aa81e3d1ed9f0
Yahia <yahimoh@icloud.com>
2023-11-10 22:13:08 +0200

    Initial UI

---
+++ Landmarks/ChildFlowView.swift
@@ -0,0 +1,92 @@
+//
+//  ChildFlowView.swift
+//  Landmarks
+//
+//  Created by Mohamad Yahia on 10.11.2023.
+//
+
+import SwiftUI
+import Charts
+
+var firstUser = UserDataModel(steps: 8898, minutesUsed: 69, minutesLeft: 21, potentialMinutes: 30)
+
+struct ChildFlowView: View {
+    var body: some View {
+        VStack {
+            VStack() {
+                Text(String(firstUser.steps))
+                    .font(.system(size: 50))
+                    .font(.title)
+                    .padding(.top, 20)
+                
+                Text("Steps done")
+                    .font(.system(size: 25))
+                    .font(.title2)
+                
+                Text(String(firstUser.minutesLeft))
+                    .font(.system(size: 50))
+                    .font(.title)
+                    .padding(.top, 20)
+                
+                Text("Minutes left")
+                    .font(.system(size: 25))
+                    .font(.title2)
+            }
+            
+            Chart {
+                BarMark(
+                    x: .value("Minutes used", firstUser.minutesUsed),
+                    y: .value("Steps left", "steps")
+                )
+            }
+            .chartXAxis(.hidden)
+            .frame(height: 40)
+            .cornerRadius(12)
+            .padding()
+
+            
+            Spacer()
+            
+            VStack {
+                
+                Button {
+                    print("Continue to app pressed")
+                } label: {
+                    Text("Continue to app")
+                        .padding()
+                        .background(Color(.systemTeal))
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                }
+                
+                Button {
+                    print("Get more time")
+                } label: {
+                    Text("Get more screen time")
+                        .padding()
+                        .background(Color(.systemTeal))
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                }
+            }
+        }
+    }
+}
+
+struct BarView: View {
+    var data: UserDataModel
+
+    var body: some View {
+        HStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: CGFloat(data.minutesLeft), height: 20)
+        }
+    }
+}
+
+
+
+#Preview {
+    ChildFlowView()
+}
---
+++ Landmarks/Models/UserDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserDataModel.swift
+//  Landmarks
+//
+//  Created by Mohamad Yahia on 10.11.2023.
+//
+
+import Foundation
+
+struct UserDataModel {
+    let steps: Int
+    let minutesUsed: Float
+    let minutesLeft: Float
+    let potentialMinutes: Float
+}
+
+
