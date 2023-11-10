e892bf4d9256839a4418d8dfb91d87f3e64c8a4d
jolow99 <jolow999@gmail.com>
2023-11-10 09:01:36 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Joseph Low on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
