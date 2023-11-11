3b525c6eee688bb38511accf00c73567b9d21f26
luukasmakila <luukas.makila@kolumbus.fi>
2023-11-11 13:53:28 +0200

    react app for Fit Time

---
+++ fit-time-react/src/App.js
@@ -0,0 +1,13 @@
+import { Header } from './components/Header';
+import { Body } from './components/Body';
+import './App.css';
+
+
+const App = () => (
+  <div className='center-div'>
+    <Header />
+    <Body />
+  </div>
+)
+
+export default App;
