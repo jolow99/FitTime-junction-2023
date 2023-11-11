cfed20250d90ba87b1e3be0af0c2c8ce4362973b
jolow99 <jolow999@gmail.com>
2023-11-11 14:43:14 +0200

    add monitor extension

---
+++ Landmarks/Utilities/TotalActivityView.swift
@@ -0,0 +1,39 @@
+//
+//  TotalActivityView.swift
+//  myDeviceActivityReportExtension
+//
+//  Created by Tarık Yılıkoğlu on 7.07.2022.
+//
+
+import SwiftUI
+
+struct TotalActivityView: View {
+    var activityReport: ActivityReport
+    
+    var body: some View {
+        VStack {
+            Spacer(minLength: 50)
+            Text("Total Screen Time")
+            Spacer(minLength: 10)
+            Text(activityReport.totalDuration.stringFromTimeInterval())
+            List(activityReport.apps) { app in
+                ListRow(eachApp: app)
+            }
+        }
+    }
+}
+
+struct ListRow: View {
+    var eachApp: AppDeviceActivity
+    var body: some View {
+        HStack {
+            Text(eachApp.displayName)
+            Spacer()
+            Text(eachApp.id)
+            Spacer()
+            Text("\(eachApp.numberOfPickups)")
+            Spacer()
+            Text(String(eachApp.duration.formatted()))
+        }
+    }
+}
